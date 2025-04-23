73537ff910f2d67f4e734ba20aed1cb342428b4d
Ariowss <arian851385@gmai.com>
2025-04-23 23:54:00 +0330

    enemy system file getting completed

--- enemy.cs
+++ enemy.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class enemy : MonoBehaviour
 { 
-    BulletPool Bullet; 
+    BulletPool pool; 
     public float speed = 5f;
     public float damage = 10f;
     public float lifeTime = 5f;
@@ -14,15 +14,18 @@ public class enemy : MonoBehaviour
     {
         Destroy(gameObject, lifeTime);
         player = GameObject.FindGameObjectWithTag("Player");
+        pool=GameObject.Find("Pool").GetComponent<BulletPool>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        GameObject bullet = Bullet.GetBullet();
-        gun1.transform.LookAt(player.transform.position);
-        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-        rb.AddForce(gun1.transform.forward * speed);
         
+        GameObject bullet = pool.GetBullet();
+        bullet.transform.position = gun1.transform.position;
+        
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.linearVelocity = (player.transform.position-gun1.transform.position).normalized * speed;
+
     }
 }
