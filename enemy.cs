3d4633720b4cd10f1302918b0cc5848282a1a5eb
Ariowss <arian851385@gmai.com>
2025-04-24 20:44:07 +0330

    fixed and updated enemy shooting system

--- enemy.cs
+++ enemy.cs
@@ -1,31 +1,48 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class enemy : MonoBehaviour
-{ 
-    BulletPool pool; 
+{
+    BulletPool pool;
     public float speed = 5f;
     public float damage = 10f;
     public float lifeTime = 5f;
+    public float shootInterval = 1f;
+    private float shootTimer;
+
     private GameObject player;
     public GameObject gun1;
-    
+
     void Start()
     {
         Destroy(gameObject, lifeTime);
         player = GameObject.FindGameObjectWithTag("Player");
-        pool=GameObject.Find("Pool").GetComponent<BulletPool>();
+        pool = GameObject.Find("Pool").GetComponent<BulletPool>();
+        shootTimer = shootInterval;
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        
-        GameObject bullet = pool.GetBullet();
-        bullet.transform.position = gun1.transform.position;
-        
-        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-        rb.linearVelocity = (player.transform.position-gun1.transform.position).normalized * speed;
+        shootTimer += Time.fixedDeltaTime;
 
+        if (shootTimer >= shootInterval)
+        {
+            Shoot();
+            shootTimer = 0f;
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = pool.GetBullet();
+        if (bullet == null) return;
+
+        bullet.transform.position = gun1.transform.position;
+        bullet.transform.rotation = Quaternion.identity;
+        bullet.SetActive(true);
+
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.linearVelocity = (player.transform.position - gun1.transform.position).normalized * speed;
     }
 }
