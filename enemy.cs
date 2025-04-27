b385fc99b2a29cdbd29228d2abe33273e250bdeb
Ariowss <arian851385@gmai.com>
2025-04-27 09:54:50 +0330

    updated enemy and player colider

--- enemy.cs
+++ enemy.cs
@@ -8,7 +8,6 @@ public class enemy : MonoBehaviour
     BulletPool pool;
     public float speed = 5f;
     public float damage = 10f;
-    public float lifeTime = 5f;
     public float FIreRate = 1f;
     private float shootTimer;
 
@@ -18,7 +17,6 @@ public class enemy : MonoBehaviour
 
     void Start()
     {
-        Destroy(gameObject, lifeTime);
         player = GameObject.FindGameObjectWithTag("Player");
         pool = GameObject.Find("Pool").GetComponent<BulletPool>();
         shootTimer = FIreRate;
@@ -43,7 +41,7 @@ public class enemy : MonoBehaviour
         bullet.transform.position = gun1.transform.position;
         bullet.transform.right = player.transform.position - transform.position;
         bullet.SetActive(true);
-        bullet.GetComponent<Bullet>().setLifeTime(lifeTime);
+        bullet.GetComponent<Bullet>().setLifeTime(bulletLifeTime);
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.linearVelocity = (player.transform.position - gun1.transform.position).normalized * speed;
     }
