f0bc41aff392f5ee2cc570ee10b4fb0a761433dd
Ariowss <arian851385@gmai.com>
2025-04-29 14:05:12 +0330

    updated health system for player

--- Health.cs
+++ Health.cs
@@ -1,24 +1,29 @@
 using System;
-using Unity.VisualScripting;
-using UnityEngine;
 
+using UnityEngine;
+using Random = UnityEngine.Random;
 public class Health : MonoBehaviour
 {
     [SerializeField] float MaxHealth = 100f;
     [SerializeField] float CurrentHealth = 100f;
     [SerializeField] GameObject Explosive;
-    
-    
+    [SerializeField] GameObject HealthPickup;
     private bool isAlive = true;
+    private float timer;
+    [SerializeField] private RectTransform TopRight;
+    [SerializeField] private RectTransform BottomLeft;
+    private GameObject heal;
+    [SerializeField] private GameObject healanim;
+    [SerializeField] private float healDuration = 100f;
+    [SerializeField] private float healAmount = 20f;
     public void Start()
     {
-
     }
-
     public float GetMaxHealth()
     {
         return MaxHealth;
     }
+    
 
     public float GetCurrentHealth()
     {
@@ -29,21 +34,14 @@ public class Health : MonoBehaviour
     {
         CurrentHealth -= damage;
     }
-
-
-
-
-    public void Heal(float heal)
-    {
-        CurrentHealth += heal;
-    }
-
-
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.name.Contains("Bullet_Player"))
+        
+        if (other.gameObject.CompareTag("Heal"))
         {
-            return;
+            CurrentHealth = Mathf.Min(CurrentHealth+healAmount, MaxHealth);
+            Instantiate(healanim, transform.position, Quaternion.identity).transform.SetParent(transform);
+            Destroy(other.gameObject);
         }
         else
         {
@@ -61,5 +59,35 @@ public class Health : MonoBehaviour
             isAlive = false;
         }
         
+    }
+
+    public GameObject MakeHeal()
+    {
+
+        Vector2 Spawnpoint;
+        Spawnpoint.x =  Random.Range(Camera.main.ScreenToWorldPoint(TopRight.position).x,Camera.main.ScreenToWorldPoint(BottomLeft.position).x);
+        Spawnpoint.y = Camera.main.ScreenToWorldPoint(TopRight.position).y+3;
+        return Instantiate(HealthPickup,Spawnpoint , Quaternion.identity);
+    }
+
+    public void Update()
+    {
+        timer += Time.deltaTime;
+        
+        if (timer >=healDuration)
+        {
+             heal=MakeHeal();
+             Debug.Log("heal");
+            timer = 0;
+        }
+
+        if (heal != null)
+        {
+            Vector2 viewPos = Camera.main.WorldToViewportPoint(heal.transform.position);
+            if ((viewPos.x > 1 || viewPos.x < 0 || viewPos.y < 0))
+            {
+                Destroy(heal);
+            }
+        }
     }
 }
